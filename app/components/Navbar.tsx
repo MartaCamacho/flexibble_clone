e21d810d196ecedb26e526515505d744d6134661
martacamacho <martacamachovargas@gmail.com>
2023-08-03 22:40:44 +0200

    navbar and footer components created and styled, authproviders component created

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,11 @@
+import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import AuthProviders from './AuthProviders'
 
 const Navbar = () => {
+    const session = {};
   return (
     <nav>
         <div className="flexBetween navbar">
@@ -16,8 +19,24 @@ const Navbar = () => {
                     />
                 </Link>
                 <ul className="xl:flex hidden text-small gap-7">
-                    <li></li>
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
                 </ul>
+            </div>
+            <div className="flexCenter gap-4">
+                {session ? (
+                    <>
+                    UserPhoto
+                        <Link href="/create-project">
+                        Share Work
+                        </Link>
+                    </>
+                ) : (
+                    <AuthProviders />
+                )}
             </div>
 
         </div>
